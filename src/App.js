a55a12b5e506a1db4e3bff235a86631c6893837a
kanna <kanna@kannas-MacBook-Pro.local>
2022-04-22 14:10:47 -0500

    rewards program initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,154 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import fetch from './api/rewardsData';
+import "./App.css";
+
+function calculateResults(incomingData) {
+  // Calculate points per transaction
+
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const pointsPerTransaction = incomingData.map(transaction => {
+    let points = 0;
+    let amountOver100 = transaction.amt - 100;
+
+    if (amountOver100 > 0) {
+      // A customer receives 2 points for every dollar spent over $100 in each transaction      
+      points += (amountOver100 * 2);
+    }
+    if (transaction.amt > 50) {
+      // plus 1 point for every dollar spent over $50 in each transaction
+      points += 50;
+    }
+    const month = new Date(transaction.transactionDt).getMonth();
+    return { ...transaction, points, month };
+  });
+
+  let byCustomer = {};
+  let totalPointsByCustomer = {};
+  pointsPerTransaction.forEach(pointsPerTransaction => {
+    let { cusId, name, month, points } = pointsPerTransaction;
+    if (!byCustomer[cusId]) {
+      byCustomer[cusId] = [];
+    }
+    if (!totalPointsByCustomer[cusId]) {
+      totalPointsByCustomer[name] = 0;
+    }
+    totalPointsByCustomer[name] += points;
+    if (byCustomer[cusId][month]) {
+      byCustomer[cusId][month].points += points;
+      byCustomer[cusId][month].monthNumber = month;
+      byCustomer[cusId][month].numTransactions++;
+    }
+    else {
+
+      byCustomer[cusId][month] = {
+        cusId,
+        name,
+        monthNumber: month,
+        month: months[month],
+        numTransactions: 1,
+        points
+      }
+    }
+  });
+  let tot = [];
+  for (var custKey in byCustomer) {
+    byCustomer[custKey].forEach(cRow => {
+      tot.push(cRow);
+    });
+  }
+  console.log("byCustomer", byCustomer);
+  console.log("tot", tot);
+  return {
+    summaryByCustomer: tot,
+    pointsPerTransaction
+  };
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [transactionData, setTransactionData] = useState(null);
+
+  useEffect(() => {
+    fetch().then((data) => {
+      const results = calculateResults(data);
+      setTransactionData(results);
+      console.log('results', results);
+    });
+  }, []);
+
+  if (transactionData == null) {
+    return <div>Loading...</div>;
+  }
+
+  return transactionData == null ?
+    <div>Loading...</div>
+    :
+    <div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-10">
+            <h2>Rewards for Each Transaction By Customers</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-8">
+            <table>
+              <tbody>
+                <tr>
+                  <th>Customer</th>
+                  <th>Transaction Date</th>
+                  <th>Amount</th>
+                  <th>Reward Points</th>
+                </tr>
+                {transactionData.pointsPerTransaction.map((val, key) => {
+                  return (
+                    <tr style={{ textAlign: 'center' }} key={key}>
+                      <td>{val.name}</td>
+                      <td>{val.transactionDt}</td>
+                      <td>{val.amt}</td>
+                      <td>{val.points}</td>
+
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-10">
+            <h2>Rewards for Each month By Customer</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-8">
+            <table>
+              <tbody>
+                <tr>
+                  <th>Customer</th>
+                  <th>Month</th>
+                  <th>Reward Points</th>
+                </tr>
+                {transactionData.summaryByCustomer.map((val, key) => {
+                  return (
+                    <tr style={{ textAlign: 'center' }} key={key}>
+                      <td>{val.name}</td>
+                      <td>{val.month}</td>
+                      <td>{val.points}</td>
+
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+    ;
 }
 
 export default App;
---
+++ src/api/rewardsData.js
@@ -0,0 +1,103 @@
+export default function rewardsData() {
+    // simulates data coming from a database.
+    return Promise.resolve(
+      [
+          {
+            cusId: 1,
+            name: "Kanna",
+            amt: 120,
+            transactionDt: "01-01-2022"
+          },
+          {
+            cusId: 1,
+            name: "Kanna",
+            amt: 100,
+            transactionDt: "01-21-2022"
+          },
+          {
+            cusId: 1,
+            name: "Kanna",
+            amt: 150,
+            transactionDt: "01-26-2022"
+          },
+          {
+            cusId: 1,
+            name: "Kanna",
+            amt: 110,
+            transactionDt: "02-01-2022"
+          },
+          {
+            cusId: 1,
+            name: "Kanna",
+            amt: 60,
+            transactionDt: "02-21-2022"
+          },
+          {
+            cusId: 1,
+            name: "Kanna",
+            amt: 180,
+            transactionDt: "03-01-2022"
+          },
+          {
+            cusId: 1,
+            name: "Kanna",
+            amt: 20,
+            transactionDt: "03-04-2022"
+          },
+          {
+            cusId: 1,
+            name: "Kanna",
+            amt: 40,
+            transactionDt: "03-15-2022"
+          },
+          {
+            cusId: 1,
+            name: "Kanna",
+            amt: 190,
+            transactionDt: "03-20-2022"
+          },
+          {
+            cusId: 2,
+            name: "Mike",
+            amt: 115,
+            transactionDt: "01-01-2022"
+          },
+          {
+            cusId: 2,
+            name: "Mike",
+            amt: 65,
+            transactionDt: "01-02-2022"
+          },
+          {
+            cusId: 2,
+            name: "Mike",
+            amt: 55,
+            transactionDt: "02-12-2022"
+          },
+          {
+            cusId: 2,
+            name: "Mike",
+            amt: 105,
+            transactionDt: "02-17-2022"
+          },
+          {
+            cusId: 2,
+            name: "Mike",
+            amt: 135,
+            transactionDt: "03-01-2022"
+          },
+          {
+            cusId: 2,
+            name: "Mike",
+            amt: 85,
+            transactionDt: "03-02-2022"
+          },
+          {
+            cusId: 3,
+            name: "Samantha",
+            amt: 120,
+            transactionDt: "02-21-2022"
+          }
+      ]
+    );
+  };
